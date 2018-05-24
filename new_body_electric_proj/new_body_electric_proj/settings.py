be8c2d59887992f79467519ebd4a35d07d21b4ff
matthewgstillman <matthewgstillman@gmail.com>
2018-05-24 11:38:00 -0700

    Reupdated Static in settings.py
    Reupdated Static in settings.py

--- new_body_electric_proj/new_body_electric_proj/settings.py
+++ new_body_electric_proj/new_body_electric_proj/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['https://newbodyelectric.herokuapp.com']
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -113,15 +114,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
 
-# Extra places for colectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static')
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
